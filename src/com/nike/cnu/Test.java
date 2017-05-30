f442a2ef9c9047991dce894825671ab83426a2b5
srinimiscuse <srinimiscuse@gmail.com>
2017-05-29 19:17:05 -0700

    second commit

--- src/com/nike/cnu/Test.java
+++ src/com/nike/cnu/Test.java
@@ -15,7 +15,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 10; i++) {
+			int j=i+1;
 			System.out.println("Hello....Welcome to Jenkins : " + i);
+			System.out.println("This is interesting : "+ j);
 		}
 
 	}
